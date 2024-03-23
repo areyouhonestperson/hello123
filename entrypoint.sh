e602ef08f8fef8fc360d9ab07cded7609b57b097
areyouhonestperson <132960299+areyouhonestperson@users.noreply.github.com>
2024-03-23 11:34:25 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="9e33d1f5-132f-4f7b-a8e2-1d16b903dbcb"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
